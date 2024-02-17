5973edc3456c70b53392297ee2533cb16fb566b6
Margarita P <rprivalko@gmail.com>
2024-02-17 15:31:06 +0300

    Updates

---
+++ precache-manifest.91e673d617500859aac917f7b509d4d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c9877454318488996f587cc077f64c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "285607f6c0ffd737a4b1",
+    "url": "/todolist/static/css/main.462c4213.chunk.css"
+  },
+  {
+    "revision": "015b740b4c299410fbf8",
+    "url": "/todolist/static/js/2.d168ddc7.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.d168ddc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "285607f6c0ffd737a4b1",
+    "url": "/todolist/static/js/main.efdfa105.chunk.js"
+  },
+  {
+    "revision": "f6ebc9bad2a2c9c79d0c",
+    "url": "/todolist/static/js/runtime-main.39f6e27a.js"
+  }
+]);
